1617808396170d30dfc3ed1d4fe8b1696b1ac951
anhpt00973 <110384434+anhpt00973@users.noreply.github.com>
2026-01-21 22:12:50 +0700

    auto create admin user

--- backend/catalog/apps.py
+++ backend/catalog/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+
+
+def create_admin():
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin", email="admin@gmail.com", password="admin"
+        )
 
 
 class CatalogConfig(AppConfig):
-    name = 'catalog'
+    name = "catalog"
+
+    def ready(self):
+        create_admin()
